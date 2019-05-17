63f3a7ca2855a1b2f59ac9a25b9b7e8531fea0d6
MahalakshmiAnandan <47468528+MahalakshmiAnandan@users.noreply.github.com>
2019-05-17 12:32:21 +0530

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class multiple{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int i=1;
+int mul=0;
+while(i<=n){
+mul=n*i;
+i++;
+System.out.println(mul);
+}
+}
+}
